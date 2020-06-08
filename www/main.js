8ed07d01c15ee3590e743c4873322edae0d38236
Yasuzawa Yuno <yasuzawa@griffin-net.co.jp>
2020-06-08 16:50:47 +0900

    message

--- www/main.js
+++ www/main.js
@@ -1,2 +1,85 @@
-const img = document.getElementById("pomu");
-const imagelist = ["83067137.png", ];
+'use strict';
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
+
+  const quizSet = shuffle([
+    {q: 'このキャラクターの名前は何でしょう？', c: ['トトロ', 'ポムポムプリン', 'ボムボムヨーグルト']},
+    {q: 'このキャラクターの名前は何でしょう？', c: ['ミルク', 'シナモン', 'ココア']},
+    {q: 'このキャラクターの名前は何でしょう？', c: ['けろけろけろっぱ', 'けろけろけろっぺ', 'けろけろけろっぴ']},
+    {q: 'このキャラクターの名前は何でしょう？', c: ['きりみちゃん', 'さかなちゃん', 'さしみちゃん']},
+    
+  ]);
+  let currentNum = 0;
+  let isAnswered;
+  let score = 0;
+
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+  function checkAnswer(li) {
+    if (isAnswered === true) {
+      return;
+    }
+    isAnswered = true;
+
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add('correct');
+      score++;
+    } else {
+      li.classList.add('wrong');
+    }
+
+    btn.classList.remove('disabled');
+  }
+
+  function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+      const li = document.createElement('li');
+      li.textContent = choice;
+      li.addEventListener('click', () => {
+        checkAnswer(li);
+      });
+      choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
+  }
+
+    setQuiz();
+
+    btn.addEventListener('click', () => {
+      if (btn.classList.contains('disabled')) {
+        return;
+      }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
+}
