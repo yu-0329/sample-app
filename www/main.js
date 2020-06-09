4bb4618b83473a21294710c73e0bf8ad5812eb78
Yasuzawa Yuno <yasuzawa@griffin-net.co.jp>
2020-06-09 15:42:55 +0900

    15:42

--- www/main.js
+++ www/main.js
@@ -2,17 +2,28 @@
 
 {
   const question = document.getElementById('question');
+  const questionImage = document.getElementById('question_image');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
   const result = document.getElementById('result');
-  const scoreLabel = document.querySelector('#result > p');
+  // const scoreLabel = document.querySelector('#result > p');
+
+  const scoreLabel = document.querySelector('#scoreLabel > p');
+  
+
+
 
   const quizSet = shuffle([
-    {q: 'このキャラクターの名前は何でしょう？', c: ['トトロ', 'ポムポムプリン', 'ボムボムヨーグルト']},
-    {q: 'このキャラクターの名前は何でしょう？', c: ['ミルク', 'シナモン', 'ココア']},
-    {q: 'このキャラクターの名前は何でしょう？', c: ['けろけろけろっぱ', 'けろけろけろっぺ', 'けろけろけろっぴ']},
-    {q: 'このキャラクターの名前は何でしょう？', c: ['きりみちゃん', 'さかなちゃん', 'さしみちゃん']},
-    
+    {q: 'このキャラクターの名前は何でしょう？', i: '../image/83067137.png', c: ['ポムポムプリン', 'トトロ', 'ボムボムヨーグルト']},
+    {q: 'このキャラクターの名前は何でしょう？', i: '../image/shinamon.png', c: ['シナモン', 'ココア', 'ミルク']},
+    {q: 'このキャラクターの名前は何でしょう？', i: '../image/kero.jpg', c: ['けろけろけろっぴ', 'けろけろけろっぺ', 'けろけろけろっぱ']},
+    {q: 'このキャラクターの名前は何でしょう？', i: '../image/kirimi2.jpg', c: ['きりみちゃん', 'さかなちゃん', 'さしみちゃん']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/retuko.jpg', c: ['アグレッシブ烈子', 'テンション高子', 'マダガスカル由美子']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/batumaru.jpg', c: ['バッドばつ丸', 'グッドばつ丸', 'グッドはな丸']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/pocha.png', c: ['ポチャッコ', 'ペチャンコ', 'ピチャッコ']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/gudetama.png', c: ['ぐでたま', 'ゆでたま', '目玉焼き']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/syr.jpg', c: ['さゆりちゃん', 'たこみちゃん', 'たこえもん']},
+    {q: 'このキャラクターの名前は何でしょう？', i:'../image/kuromi.png', c: ['クロミ', 'マイメロディ', 'ハローキティ']},
   ]);
   let currentNum = 0;
   let isAnswered;
@@ -46,6 +57,7 @@
   function setQuiz() {
     isAnswered = false;
     question.textContent = quizSet[currentNum].q;
+    questionImage.src = quizSet[currentNum].i;
 
     while(choices.firstChild) {
       choices.removeChild(choices.firstChild);
@@ -75,11 +87,17 @@
     btn.classList.add('disabled');
 
     if (currentNum === quizSet.length - 1) {
-      scoreLabel.textContent = `score: ${score} / ${quizSet.length}`;
       result.classList.remove('hidden');
+      let total = Math.floor(score / quizSet.length * 100);
+      console.log(total);
+      scoreLabel.textContent = total;
+      
     } else {
       currentNum++;
       setQuiz();
     }
   });
+
+  
+
 }
